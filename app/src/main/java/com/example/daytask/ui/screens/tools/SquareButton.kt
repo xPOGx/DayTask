4df0b892814073136f40c16f80be3cb8134ddb80
POG <pavel19.1078@gmail.com>
2023-11-08 15:46:35 +0200

    No click animation

--- app/src/main/java/com/example/daytask/ui/screens/tools/SquareButton.kt
+++ app/src/main/java/com/example/daytask/ui/screens/tools/SquareButton.kt
@@ -3,10 +3,14 @@ package com.example.daytask.ui.screens.tools
 import androidx.annotation.DimenRes
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
@@ -21,11 +25,17 @@ fun SquareButton(
     @DrawableRes iconRes: Int,
     tint: Color = Color.Unspecified
 ) {
-    IconButton(
-        onClick = onClick,
+    Box(
+        contentAlignment = Alignment.Center,
         modifier = modifier
+            .minimumInteractiveComponentSize()
             .background(MainColor)
             .size(dimensionResource(sizeRes))
+            .clickable(
+                onClick = onClick,
+                interactionSource = MutableInteractionSource(),
+                indication = null
+            )
     ) {
         Icon(
             painter = painterResource(iconRes),
